08c6239f85716886383ea4a0fda94ea142fc1719
Anton Wilhelm <timaschew@users.noreply.github.com>
2018-04-24 14:50:02 +0200

    Distinguish between local and external files for URL transformation for javadoc

--- javadoc.js
+++ javadoc.js
@@ -1,13 +1,22 @@
 // handler for javadoc and scaladoc
-module.exports = function(href, packagePrefix) {
-	href = href.replace(/(serialized-form|index)\.html[\?#]([^\s\\^@^"]*)([^\s\\^"]*)/g, (match, _, g1, g2) => {
-		if (g1.indexOf(packagePrefix) == -1) {
-			return match
+module.exports = function(href, local, externalUrls = []) {
+	href = href.replace(/(.*)(serialized-form|index)\.html[\?#]([^\s\\^@^"]*)([^\s\\^"]*)/g, (match, prefix, _, g1, g2) => {
+		if (prefix.indexOf('http') == 0) {
+			if (externalUrls.some(u => prefix.indexOf(u) != -1)) {
+				// transform
+			} else {
+				// ignore
+				return match
+			}
+		} else {
+			if (!local) {
+				return match
+			}
 		}
 		const elements = g1.replace(/\.html/, '').split('.')
 		const packages = elements.filter(l => l == l.toLowerCase() && l != 'html').join('/')
 		const className = elements.filter(l => l != l.toLowerCase() && l != 'html').join('.')
-        return packages + (className != '' ? '/' + className  : '') + '.html' + (g2 || '').replace('@', '#')
+        return prefix + packages + (className != '' ? '/' + className  : '') + '.html' + (g2 || '').replace('@', '#')
     })
     return href
 }
