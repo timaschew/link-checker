075f6de2615749909fa655f5a3e378260de5a383
Pomax <pomax@nihongoresources.com>
2020-09-18 11:19:11 -0700

    - Add error line/column information - Add force-zero-exit-code CLI flag

--- link-checker.js
+++ link-checker.js
@@ -15,13 +15,15 @@ var rcConfig = require('rc')('linkchecker', {});
 const CACHE_FILE = 'cache-v1.json'
 
 module.exports = function(directory, options = {}, callback) {
-    options = {...rcConfig, ...options}
+	options = {...rcConfig, ...options}
 
 	let cache = null
 	const expiration = ms(options.httpCacheMaxAge || '1w')
 	if (expiration == null) {
 		console.error('Invalid value for --http-cache-max-age')
-		process.exit(1)
+		if (!options.forceZeroExitCode) {
+			process.exit(1)
+		}
 	}
 	if (options.httpCache != null && options.httpCache != '') {
 		console.log('Using cache directory', options.httpCache, 'with max age of', expiration, 'seconds')
@@ -32,17 +34,17 @@ module.exports = function(directory, options = {}, callback) {
 		} catch(err) {
 			cache = {}
 		}
-    }
-    overridePatterns = new Map(Object.entries(options.overrides || {}))
-    options.overrides = new Map()
-    overridePatterns.forEach((opts, pattern) => {
-        options.overrides.set(new RegExp(pattern), opts)
-        options.overrides.delete(pattern)
-    })
-	
+	}
+	overridePatterns = new Map(Object.entries(options.overrides || {}))
+	options.overrides = new Map()
+	overridePatterns.forEach((opts, pattern) => {
+		options.overrides.set(new RegExp(pattern), opts)
+		options.overrides.delete(pattern)
+	})
+
 	const localLinks = new Map() // links to other local files, without an anchor
 	const localAnchorLinks = new Map() // links to other local files with an anchor
-	const localParentLinks = new Map() // 
+	const localParentLinks = new Map() //
 	const localParentAnchorLinks = new Map()
 	const remoteLinks = new Map() // links to remote files, http(s), without an anchor)
 	const remoteAnchorLinks = new Map() // links to remote files, http(s) with an anchor
@@ -52,21 +54,29 @@ module.exports = function(directory, options = {}, callback) {
 	const errors = []
 
 	let fileCounter = 0
-    debug('scanning directory', directory)
-    
-    function getOverrideFor(target) {
-        const url = target instanceof Error && target.response ? target.response.request.url // superagent error
-            : typeof target === "object" ? target.request.url // superagent response
-            : typeof target === "string" ? target // plain URL
-            : null
+	debug('scanning directory', directory)
 
-        const matchingPattern = Array.from(options.overrides.keys()).find(
-            pattern => pattern.exec(url)
-        )
-        if(!matchingPattern) return options
+	function getOverrideFor(target) {
+		const url = target instanceof Error && target.response && target.response.request? target.response.request.url // superagent error
+				  : typeof target === "object" && target.request ? target.request.url // superagent response
+				  : typeof target === "string" ? target // plain URL
+				  : undefined
 
-        return {...options, ...options.overrides.get(matchingPattern)}
-    }
+		if (url === undefined) {
+			console.log('unknown error for:', JSON.stringify(target));
+			if (!options.forceZeroExitCode) {
+				process.exit(1);
+			}
+			return options;
+		}
+
+		const matchingPattern = Array.from(options.overrides.keys()).find(
+			pattern => pattern.exec(url)
+		)
+		if(!matchingPattern) return options
+
+		return {...options, ...options.overrides.get(matchingPattern)}
+	}
 
 	walker(directory, function(filePath, fileContent) {
 		if (filePath == '') {
@@ -75,14 +85,36 @@ module.exports = function(directory, options = {}, callback) {
 		fileCounter += 1
 		localPages.add(filePath)
 
-		const $ = cheerio.load(fileContent)
+
+		const lines = fileContent.split('\n');
+		const lineStarts = [];
+		for(let i=0; i<lines.length; i++) {
+			if (i===0) {
+				lineStarts[i] = 0;
+				continue;
+			}
+			lineStarts[i] = lineStarts[i-1] + 1 + lines[i].length;
+		};
+
+		const getLineCol = function(pos) {
+			let line = lineStarts.findIndex(function(v) {
+				return v > pos;
+			}) - 1;
+			return [line, pos - lineStarts[line]];
+		};
+
+		const $ = cheerio.load(fileContent, {
+			xmlMode: true,
+			withStartIndices: true
+		});
 
 		const links = $('body').find('a')
 		links.each(function(i, element) {
+			const position = getLineCol(element.startIndex);
 			const $this = $(this)
-            let href = ($this.attr('href') || '').trim()
-            const linkSpecificOptions = getOverrideFor(href)
-			
+			let href = ($this.attr('href') || '').trim()
+			const linkSpecificOptions = getOverrideFor(href)
+
 			if (href.indexOf('mailto:') == 0) {
 				return
 			}
@@ -105,7 +137,7 @@ module.exports = function(directory, options = {}, callback) {
 					debug('ignoring file', filePath)
 					return
 				}
-			}	
+			}
 
 			if (href == '.') {
 				debug('ignore link to itself via . from', filePath)
@@ -118,7 +150,7 @@ module.exports = function(directory, options = {}, callback) {
 
 			if (linkSpecificOptions['url-swap'] && linkSpecificOptions['url-swap'].length > 0) {
 				const found = linkSpecificOptions['url-swap'].forEach(line => {
-					// DO NOT use split(':') because it might be replaced with http:// 
+					// DO NOT use split(':') because it might be replaced with http://
 					const indexOfColon = line.indexOf(':')
 					const pattern = new RegExp(line.substr(0, indexOfColon))
 					const replacement = line.substr(indexOfColon + 1)
@@ -128,7 +160,7 @@ module.exports = function(directory, options = {}, callback) {
 						debug('replaced', href)
 					}
 				})
-			}	
+			}
 			if (linkSpecificOptions['url-ignore'] && linkSpecificOptions['url-ignore'].length > 0) {
 				const found = linkSpecificOptions['url-ignore'].some(ignore => {
 					return href.match(ignore) != null
@@ -158,9 +190,9 @@ module.exports = function(directory, options = {}, callback) {
 					debug('ignore remote link' + href, filePath)
 					return
 				}
-				
+
 			}
-			
+
 			if (options.javadoc || (options['javadoc-external'] && options['javadoc-external'].length > 0)) {
 				href = javadoc(href, options.javadoc, options['javadoc-external'])
 				// some links have a special href attribute (<a xlink:href="...">)
@@ -192,15 +224,16 @@ module.exports = function(directory, options = {}, callback) {
 			debug('text content for ' + resolvedHref, $this.html())
  			if (href.indexOf('http://') == 0 || href.indexOf('https://') == 0) {
 				if (href.indexOf('#') == -1) {
-					remoteLinks.set(href, filePath)
+					remoteLinks.set(href, { filePath, position })
 				} else {
-					remoteAnchorLinks.set(href, filePath)
+					remoteAnchorLinks.set(href, { filePath, position })
 				}
 			} else if (resolvedHref.indexOf('..') == 0) {
 				// non http(s) links
 				if (options['limit-scope']) {
 					// TODO: same error will reported multiple times, consider to do the check and creating errors in the callback/
 					errors.push({
+
 						type: 'out-of-scope',
 						target: resolvedHref,
 						source: filePath,
@@ -208,26 +241,24 @@ module.exports = function(directory, options = {}, callback) {
 					})
 				} else {
 					if (href.indexOf('#') == -1) {
-						localParentLinks.set(resolvedHref, filePath)
+						localParentLinks.set(resolvedHref, { filePath, position })
 					} else {
-						localParentAnchorLinks.set(resolvedHref, filePath)
+						localParentAnchorLinks.set(resolvedHref, { filePath, position })
 					}
 				}
 			} else if (href.indexOf('#') != -1) {
 				const resolvedAnchorHref = (href.indexOf('#') == 0 ? filePath + href : resolvedHref)
 				debug('adding localAnchorLink on page ' + filePath, resolvedAnchorHref)
-				localAnchorLinks.set(resolvedAnchorHref, filePath) // consider to use a set as value
+				localAnchorLinks.set(resolvedAnchorHref, { filePath, position })
 			} else {
 				debug('adding localLink on page ' + filePath, resolvedHref)
-				localLinks.set(resolvedHref, filePath) // consider to use a set as value
+				localLinks.set(resolvedHref, { filePath, position })
 			}
-
-
-
 		})
 
 		const anchors = $('html').find('[id], [name]')
 		anchors.each(function(i, element) {
+			const position = getLineCol(element.startIndex);
 			const $this = $(this)
 			const anchor = $this.attr('id') || $this.attr('name')
 			const entry = localAnchors.get(filePath) || new Set()
@@ -235,7 +266,10 @@ module.exports = function(directory, options = {}, callback) {
 			debug('adding new local anchor on page ' + filePath, anchor)
 			localAnchors.set(filePath, entry)
 			if (options['warn-name-attr'] && $this.attr('name')) {
+				const lineCol = getLineCol(element.startIndex);
 				warnings.push({
+					line: lineCol[0],
+					column: lineCol[1],
 					type: 'anchor',
 					content: anchor,
 					source: filePath,
@@ -243,7 +277,7 @@ module.exports = function(directory, options = {}, callback) {
 				})
 			}
 		})
-		
+
 	}, async function() {
 		debug('localPages', localPages)
 		debug('remotePages', remoteLinks)
@@ -251,10 +285,13 @@ module.exports = function(directory, options = {}, callback) {
 		debug('localAnchorLinks', localAnchorLinks)
 		debug('localAnchors', localAnchors)
 
-		localLinks.forEach((sourcePage, link) => {
+		localLinks.forEach((details, link) => {
+			const sourcePage = details.filePath
 			if (localPages.has(link) === false) {
 				debug('page not found from', sourcePage, 'to', link)
 				errors.push({
+					line: details.position[0],
+					column: details.position[1],
 					type: 'page',
 					target: link,
 					source: sourcePage,
@@ -263,9 +300,10 @@ module.exports = function(directory, options = {}, callback) {
 			}
 		})
 
-		localAnchorLinks.forEach((sourcePage, link) => {
+		localAnchorLinks.forEach((details, link) => {
 			debug('lookup for', link)
-			const anchorCharIndex = link.indexOf('#') 
+			const sourcePage = details.filePath;
+			const anchorCharIndex = link.indexOf('#')
 			const page = link.substr(0, anchorCharIndex)
 			const anchor = link.substr(anchorCharIndex + 1)
 			const resolvedPage = page === '' ? sourcePage : page
@@ -273,6 +311,8 @@ module.exports = function(directory, options = {}, callback) {
 			if (entry.has(anchor) === false) {
 				debug('anchor not found from', sourcePage, 'to', link)
 				errors.push({
+					line: details.position[0],
+					column: details.position[1],
 					type: 'anchor',
 					target: resolvedPage + '#' + anchor,
 					anchor: anchor,
@@ -283,35 +323,31 @@ module.exports = function(directory, options = {}, callback) {
 		})
 
 		const localParentLinksArray = Array.from(localParentLinks.keys())
-		await Promise.all(localParentLinksArray.map(target => {
-			return new Promise((resolve, reject) => {
-				fileCounter += 1
-				fs.exists(path.resolve(directory, target), result => {
-					resolve(result)
+		const localParentFilesExist = localParentLinksArray.map(target => {
+			fileCounter += 1
+			return fs.existsSync(path.resolve(directory, target))
+		})
+		localParentFilesExist.forEach((exists, index) => {
+			const target = localParentLinksArray[index]
+			const source = localParentLinks.get(target)
+			if (exists === false) {
+				errors.push({
+					line: source.position[0],
+					column: source.position[1],
+					type: 'page',
+					target: target,
+					source: source.filePath,
+					reason: 'page not found'
 				})
-			})
-		}))
-		.then(results => {
-			results.forEach((result, index) => {
-				const target = localParentLinksArray[index]
-				const source = localParentLinks.get(target)
-				if (result == false) {
-					errors.push({
-						type: 'page',
-						target: target,
-						source: source,
-						reason: 'page not found'
-					})
-				}
-			})
+			}
 		})
 
 		const localParentAnchorLinksArray = Array.from(localParentAnchorLinks.keys())
 		await Promise.all(localParentAnchorLinksArray.map(target => {
 			return new Promise((resolve, reject) => {
 				// fileCounter += 1 // TODO: count but not the same page several times
-				const filepath = target.split('#')[0] // ignore the anchor
-				fs.readFile(path.resolve(directory, filepath), 'utf8', resolve)
+				const filePath = target.split('#')[0] // ignore the anchor
+				fs.readFile(path.resolve(directory, filePath), 'utf8', resolve)
 			})
 		})
 		// map rejected to resolved promises
@@ -322,27 +358,31 @@ module.exports = function(directory, options = {}, callback) {
 				const source = localParentAnchorLinks.get(target)
 				if (result instanceof Error) {
 					return errors.push({
+						line: source.position[0],
+						column: source.position[1],
 						type: 'page',
 						target: target,
-						source: source,
+						source: source.filePath,
 						reason: 'page not found'
 					})
 				}
 
 				if (result == false) {
 					errors.push({
+						line: source.position[0],
+						column: source.position[1],
 						type: 'anchor',
 						target: target,
-						source: source,
+						source: source.filePath,
 						reason: 'anchor not found'
 					})
 				}
 			})
-        })
+		})
 
 		const remoteLinksArray = Array.from(remoteLinks.keys())
 		await Promise.all(remoteLinksArray.map(target => {
-            const linkSpecificOptions = getOverrideFor(target)
+			const linkSpecificOptions = getOverrideFor(target)
 			let method = 'head'
 			if (linkSpecificOptions['http-always-get']) {
 				method = 'get'
@@ -356,7 +396,7 @@ module.exports = function(directory, options = {}, callback) {
 		.map(p => p.catch(error => error)))
 		.then(responses => {
 			responses.forEach((response, index) => {
-                const linkSpecificOptions = getOverrideFor(response)
+				const linkSpecificOptions = getOverrideFor(response)
 				if (response && response.statusCode && response.statusCode >= 200 && response.statusCode < 300) {
 					// ok
 					const target = remoteLinksArray[index]
@@ -386,9 +426,11 @@ module.exports = function(directory, options = {}, callback) {
 						statusCodeText = ` (Code: ${statusCode})`
 					}
 					errors.push({
+						line: source.position[0],
+						column: source.position[1],
 						type: 'remote-page',
 						target: target,
-						source: source,
+						source: source.filePath,
 						reason: 'could not fetch external page: ' + error.toString() + statusCodeText
 					})
 				}
@@ -400,42 +442,44 @@ module.exports = function(directory, options = {}, callback) {
 
 		const remoteAnchorLinksArray = Array.from(remoteAnchorLinks.keys())
 		await Promise.all(remoteAnchorLinksArray.map(target => {
-            const linkSpecificOptions = getOverrideFor(target)
+			const linkSpecificOptions = getOverrideFor(target)
 			if (cache && cache[target] && cache[target].created + expiration > Date.now()) {
 				return new Promise(resolve => resolve(Object.assign({}, cache[target].payload, {cached: true})))
-                        }
+						}
 			return agent.get(target).timeout({response: linkSpecificOptions['http-timeout']}).redirects(linkSpecificOptions['http-redirects'])
 		})
 		// map rejected to resolved promises
 		.map(p => p.catch(error => error)))
 		.then(responses => {
 			responses.forEach((response, index) => {
-                if(!response.request && !response.response.request) {
-                    console.error(response)
-                }
-                const linkSpecificOptions = getOverrideFor(response)
+				if(!response.request && !response.response.request) {
+					console.error(response)
+				}
+				const linkSpecificOptions = getOverrideFor(response)
 				const target = remoteAnchorLinksArray[index]
 				const source = remoteAnchorLinks.get(target)
 
 				if (response && response.statusCode && response.statusCode >= 200 && response.statusCode < 300) {
 					if (cache && !response.cached) {
 						cache[target] = {
-        	                                        payload: {
-                	                                        statusCode: response.statusCode,
-								text: response.text 
-                                	                },
-                                        	        created: Date.now()
-                                        	}
-                    }
-                    if(!linkSpecificOptions['allow-hash-ref']) return
+													payload: {
+															statusCode: response.statusCode,
+								text: response.text
+													},
+													created: Date.now()
+											}
+					}
+					if(!linkSpecificOptions['allow-hash-ref']) return
 					const anchor = target.split('#')[1]
 					const $ = cheerio.load(response.text)
 					const anchors = $('body').find(`[id='${anchor}'], [name='${anchor}']`)
 					if (anchors.length == 0) {
 						errors.push({
+							line: source.position[0],
+							column: source.position[1],
 							type: 'remote-anchor',
 							target: target,
-							source: source,
+							source: source.filePath,
 							reason: 'page was found but not the anchor'
 						})
 					}
@@ -456,9 +500,11 @@ module.exports = function(directory, options = {}, callback) {
 						statusCodeText = ` (Code: ${statusCode})`
 					}
 					errors.push({
+						line: source.position[0],
+						column: source.position[1],
 						type: 'remote-anchor',
 						target: target,
-						source: source,
+						source: source.filePath,
 						reason: 'could not fetch external page: ' + error.toString() + statusCodeText
 					})
 				}
@@ -487,4 +533,3 @@ module.exports = function(directory, options = {}, callback) {
 		})
 	})
 }
-
